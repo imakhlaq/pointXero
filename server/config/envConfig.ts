5851d47c4781182b5afbf1e18212d520d35eb9e0
imakhlaq <imakhlaqXD@gmail.com>
2023-06-13 20:02:45 +0530

    jwt authentication

--- server/config/envConfig.ts
+++ server/config/envConfig.ts
@@ -5,6 +5,7 @@ dotenv.config();
 const config = {
   PORT: process.env.PORT || 5000,
   DB_URL: process.env.DB_URL,
+  SECRET_KEY: process.env.SECRET_KEY,
 };
 
 export default config;
--- server/db/schema/users.ts
+++ server/db/schema/users.ts
@@ -1,12 +1,31 @@
-import { pgTable, serial, text, varchar, uuid } from 'drizzle-orm/pg-core';
+import { InferModel } from 'drizzle-orm';
+import {
+  pgTable,
+  serial,
+  text,
+  varchar,
+  uuid,
+  timestamp,
+  pgEnum,
+} from 'drizzle-orm/pg-core';
 
-export const users2 = pgTable('users', {
-  id: serial('id').primaryKey(),
-  fullName: text('full_name'),
-  phone: varchar('phone', { length: 256 }),
-});
+// declaring enum in database
+export const roleEnum = pgEnum('role', ['user', 'seller', 'admin']);
 
-export const users = pgTable('users', {
+export const user = pgTable('users', {
   id: uuid('id').defaultRandom().primaryKey().notNull(),
-  firstName: varchar('name', { length: 256 }).notNull(),
+  userName: varchar('user_name', { length: 25 }).notNull(),
+  firstName: varchar('first_name', { length: 150 }).notNull(),
+  lastName: varchar('last_name', { length: 150 }).notNull(),
+  password: varchar('password').notNull(),
+  phone: varchar('phone', { length: 20 }).notNull(),
+  email: varchar('email', { length: 30 }).notNull(),
+  role: roleEnum('role').default('user'),
+  forgetPassToken: varchar('forget_pass_token'),
+  forgetPassTokenValidTill: timestamp('forgetPassTokenValidTill'),
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+  updatedAt: timestamp('updated_at').defaultNow().notNull(),
 });
+
+export type NewUser = InferModel<typeof user, 'insert'>; // insert type
+export type User = InferModel<typeof user, 'select'>; // insert type
