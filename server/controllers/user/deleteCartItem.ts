77d5aa9daf291c8398fef9a2a69e442910291430
imakhlaq <imakhlaqXD@gmail.com>
2023-06-21 08:05:49 +0530

    cart implemented

---
+++ server/controllers/user/deleteCartItem.ts
@@ -0,0 +1,55 @@
+import { Request, Response } from "express";
+import { prisma } from "../../db/database";
+import CustomError from "../../utils/CustomError";
+import formatError from "../../utils/formatError";
+
+async function deleteCartItem(req: Request, res: Response) {
+  const { prodId } = req.params;
+  const quantity = Number(req.query.quantity) ?? 1;
+
+  try {
+    const cart = await prisma.cart.findUnique({
+      where: { userId: req.body.userId },
+      include: {
+        CartItem: true,
+      },
+    });
+
+    if (!cart) {
+      throw new CustomError("Cart doesn't exist", 404);
+    }
+
+    const cartItem = cart.CartItem.find((item) => item.productId === prodId);
+
+    if (!cartItem) {
+      throw new CustomError("Product doesn't exist", 404);
+    }
+
+    const newQuantity = cartItem.quantity - quantity;
+
+    if (newQuantity <= 0) {
+      const updateQuantity = await prisma.cartItem.delete({
+        where: { id: cartItem.id },
+      });
+      return res
+        .status(200)
+        .json({ message: `${updateQuantity.id} is deleted}` });
+    } else {
+      const updateQuantity = await prisma.cartItem.update({
+        where: { id: cartItem.id },
+        data: {
+          quantity: newQuantity,
+        },
+      });
+
+      return res
+        .status(200)
+        .json({ message: `${updateQuantity.id} is deleted` });
+    }
+  } catch (_err) {
+    const err = _err as CustomError;
+    return res.status(404).json(formatError(err));
+  }
+}
+
+export default deleteCartItem;
--- server/routes/userRouter.ts
+++ server/routes/userRouter.ts
@@ -3,11 +3,13 @@ import userAuth from "../middlewares/userAutth";
 import addProduct from "../controllers/user/addProduct";
 import getCartData from "../controllers/user/getCartData";
 import updateCart from "../controllers/user/updateCart";
+import deleteCartItem from "../controllers/user/deleteCartItem";
 
 const app = Router();
 
 app.post("/addproduct", userAuth, addProduct);
 app.get("/getcart", userAuth, getCartData);
 app.post("/updatecart", userAuth, updateCart);
+app.get("/deletecartitem/:prodId", userAuth, deleteCartItem);
 
 export default app;
