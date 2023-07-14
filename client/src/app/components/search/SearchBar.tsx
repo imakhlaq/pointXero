10a8f99fa17615ffff78637c62071b69bbad6e26
imakhlaq <imakhlaqXD@gmail.com>
2023-07-14 13:37:54 +0530

    fiexes

--- client/src/app/components/search/SearchBar.tsx
+++ client/src/app/components/search/SearchBar.tsx
@@ -1,19 +1,40 @@
+"use client";
 import { RiSearchLine } from "react-icons/ri";
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
+import {FormEvent} from "react";
+
+type T = {
+  e: React.FormEvent | React.MouseEvent<HTMLButtonElement, MouseEvent>;
+};
 
 const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const router = useRouter();
 
+  function submitHandler1<T>(e: T) {
+
+
+  }
 
   return (
-    <div className="flex justify-center items-center rounded-sm p-2 flex-1 w-full">
-      <div className="bg-inherit p-1 border border-gray border-r-0 rounded-l-md min-h-11 w-2/3 lg:w-[28rem]">
-        <input
-          type="text"
-          className="focus-within:bg-none focus:outline-none bg-inherit px-3 text-whiteText w-full"
-          placeholder="Search"
-        />
+    <div className="flex justify-center items-center rounded-sm p-2 flex-1 w-full }">
+      <div className="bg-inherit border border-gray border-r-0 rounded-l-md min-h-11 w-2/3 lg:w-[28rem] rounded">
+        <form onSubmit={event => {}}>
+          <input
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            type="text"
+            className="focus-within:bg-none focus:outline-none bg-inherit px-3 text-whiteText w-full py-2 focus:bg-whiteText rounded focus:text-black"
+            placeholder="Search"
+          />
+        </form>
       </div>
 
-      <button className="bg-greenColor px-4 py-2.5 rounded-r-md min-h-11 overflow-hidden">
+      <button
+        onClick={(event) => {}}
+        className="bg-greenColor px-4 py-2.5 rounded-r-md min-h-11 overflow-hidden"
+      >
         <RiSearchLine className=" h-4 w-4 text-cyan-50 text-center" />
       </button>
     </div>
--- client/src/app/components/ui/Item.tsx
+++ client/src/app/components/ui/Item.tsx
@@ -1,5 +1,6 @@
 import { CategoryProduct } from "../../../../types";
 import BuyNowAddToCart from "@/app/components/ui/BuyNowAddToCart";
+import Link from "next/link";
 
 type Props = {
   data: CategoryProduct;
@@ -14,28 +15,32 @@ const Item = ({ data }: Props) => {
     100;
 
   return (
-    <div className="bg-blueBgColor p-3 rounded relative ">
-      <img
-        alt={data.title}
-        src={data.image[0].url}
-        className="max-w-[13rem] md:max-w-[15rem] rounded "
-      />
+    <Link href={`/product/${data.id}`}>
+      <div className="bg-blueBgColor p-3 rounded relative ">
+        <img
+          alt={data.title}
+          src={data.image[0].url}
+          className="max-w-[13rem] md:max-w-[15rem] rounded "
+        />
 
-      <h3>{data.title}</h3>
-      <div className="flex items-center gap-2">
-        <p className="text-sm">
-          ₹
-          <span className="text-lg md:text-2xl">
-            {data.versions[0].currentPrice}
-          </span>
-        </p>
-        <p className="line-through text-sm self-end">
-          M.R.P: {data.versions[0].marketPrice}
-        </p>
-        <p className=" text-sm self-end">({discount.toFixed(0)}% off)</p>
+        <h3 className="mt-2 md:mt-4 text-lg md:text-xl mb-2 max-w-[14rem]">
+          {data.title}
+        </h3>
+        <div className="flex items-center gap-2">
+          <p className="text-sm">
+            ₹
+            <span className="text-lg md:text-2xl">
+              {data.versions[0].currentPrice}
+            </span>
+          </p>
+          <p className="line-through text-sm self-end text-gray">
+            M.R.P: {data.versions[0].marketPrice}
+          </p>
+          <p className=" text-sm self-end">({discount.toFixed(0)}% off)</p>
+        </div>
+        <BuyNowAddToCart />
       </div>
-      <BuyNowAddToCart />
-    </div>
+    </Link>
   );
 };
 export default Item;
