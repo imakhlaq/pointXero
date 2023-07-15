cba396dbeee430b49b94fa8bb50a13a8bdfaf389
imakhlaq <imakhlaqXD@gmail.com>
2023-07-16 00:29:30 +0530

    fiexed search box

--- client/src/app/components/search/SearchBar.tsx
+++ client/src/app/components/search/SearchBar.tsx
@@ -1,41 +1,39 @@
 "use client";
 import { RiSearchLine } from "react-icons/ri";
-import React, { useState } from "react";
+import React, { FormEvent, useState } from "react";
 import { useRouter } from "next/navigation";
-import {FormEvent} from "react";
-
-type T = {
-  e: React.FormEvent | React.MouseEvent<HTMLButtonElement, MouseEvent>;
-};
 
 const SearchBar = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const router = useRouter();
 
-  function submitHandler1<T>(e: T) {
+  //generic to handle the search click
+  function submitHandler1<T extends FormEvent<HTMLFormElement>>(e?: T): void {
+    if (e) e.preventDefault();
+    if (!searchTerm.trim().length) return;
 
-
+    router.push(`/search/${searchTerm}`);
   }
 
   return (
-    <div className="flex justify-center items-center rounded-sm p-2 flex-1 w-full }">
-      <div className="bg-inherit border border-gray border-r-0 rounded-l-md min-h-11 w-2/3 lg:w-[28rem] rounded">
-        <form onSubmit={event => {}}>
+    <div className="flex justify-center items-center p-2 flex-1 w-full }">
+      <div className="bg-inherit border border-gray border-r-0 min-h-11 w-2/3 lg:w-[28rem] rounded-l-md">
+        <form onSubmit={submitHandler1}>
           <input
             value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}
             type="text"
-            className="focus-within:bg-none focus:outline-none bg-inherit px-3 text-whiteText w-full py-2 focus:bg-whiteText rounded focus:text-black"
+            className="focus-within:bg-none focus:outline-none bg-inherit px-3 text-whiteText w-full py-2 focus:bg-whiteText  focus:text-black"
             placeholder="Search"
           />
         </form>
       </div>
 
       <button
-        onClick={(event) => {}}
-        className="bg-greenColor px-4 py-2.5 rounded-r-md min-h-11 overflow-hidden"
+        onClick={(e) => submitHandler1()}
+        className="bg-greenColor px-4 py-3 rounded-r-full min-h-11 overflow-hidden"
       >
-        <RiSearchLine className=" h-4 w-4 text-cyan-50 text-center" />
+        <RiSearchLine className="h-[1.1rem] w-5 text-cyan-50 text-center" />
       </button>
     </div>
   );
